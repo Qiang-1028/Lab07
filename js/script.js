5e4e3466c699dc4867067e12e7af4880c0e847fd
Qiang-1028 <54606089+Qiang-1028@users.noreply.github.com>
2019-11-20 22:02:25 -0600

    1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', init);
 function init(){
-document.getElementById("entrybutton").addEventListener("click", myfunction);
+  document.getElementById("entrybutton").addEventListener("click", myfunction);
 	function myfunction() {
 		var message = document.getElementById("entryinput").value;
 		alert("Qiang Bao: " + message);
